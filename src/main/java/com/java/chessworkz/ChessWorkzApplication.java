fac48fa8eb9ccbfd9796f54648d24cd7c58ed37e
David Chang <Davidchang@live.nl>
2022-09-12 10:24:32 +0200

    Adding correct code

---
+++ src/main/java/com/java/chessworkz/ChessWorkzApplication.java
@@ -0,0 +1,13 @@
+package com.java.chessworkz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessWorkzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessWorkzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/chessworkz/ChessWorkzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.chessworkz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessWorkzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
