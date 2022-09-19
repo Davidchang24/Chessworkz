b1654f37e6235b8a8717379fe69b2be738d4f371
David Chang <Davidchang@live.nl>
2022-09-19 13:15:14 +0200

    Added data.sql file and local application.properties settings

--- src/main/java/com/java/chessworkz/controller/CourseController.java
+++ src/main/java/com/java/chessworkz/controller/CourseController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
