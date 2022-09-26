0dca41e505e3cbbda6104a25765cc6fa1ca4e68b
David Chang <Davidchang@live.nl>
2022-09-26 16:08:59 +0200

    deleted sql file that caused problems with testing in ci

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO course (id, course_description, course_name)
-VALUES (1, 'This chess course is about openings', 'Openings'),
-       (2, 'This chess course is about middlegames', 'Middlegames'),
-       (3, 'This chess course is about endgames', 'Endgames');
