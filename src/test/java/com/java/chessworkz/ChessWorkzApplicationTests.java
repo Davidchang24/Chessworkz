f28c737501055a23bc6c36dda64959baede25b93
Davidchang24 <Davidchang@live.nl>
2023-01-15 12:37:14 +0100

    updates

--- src/test/java/com/java/chessworkz/ChessWorkzApplicationTests.java
+++ src/test/java/com/java/chessworkz/ChessWorkzApplicationTests.java
@@ -3,9 +3,11 @@ package com.java.chessworkz;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@EnableJpaRepositories(basePackages = {"com.java.chessworkz"})
+@EnableJpaRepositories(basePackages = { "com.java.chessworkz" })
+@TestPropertySource(locations = "classpath:test.properties")
 class ChessWorkzApplicationTests {
 
     @Test
