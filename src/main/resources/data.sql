1a70a0bf4f9a6060d4c7b7ea9fff2cc9b6a25aae
Davidchang24 <Davidchang@live.nl>
2023-01-15 11:50:44 +0100

    Added sql seeder

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO course (id, course_Description, course_Name) VALUES (1, 'This course is about openings', 'Openings');
+INSERT INTO course (id, course_Description, course_Name) VALUES (2, 'This course is about middlegames', 'Middlegames');
+INSERT INTO course (id, course_Description, course_Name) VALUES (3, 'This course is about endgames', 'endgames');
